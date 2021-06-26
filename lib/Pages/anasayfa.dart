22cbf41db4b8ca2570d7c2cc750311d40e6e1c4e
mfccengiz <cengiz.mfc@gmail.com>
2021-06-27 00:07:56 +0300

    notlar 3

--- lib/Pages/anasayfa.dart
+++ lib/Pages/anasayfa.dart
@@ -2,6 +2,7 @@ import 'package:projeqr/Pages/main.dart';
 import 'package:flutter/material.dart';
 
 //sa merhaba
+//bugün baya bir şey öğrendik
 class AnaSayfa extends StatefulWidget {
   @override
   _AnaSayfaState createState() => _AnaSayfaState();
